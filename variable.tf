2e863ef1a927c43051bce41ee8e0f62c35291bd4
Corey Masters <corey.masters@imaginexconsulting.com>
2021-03-16 11:04:20 -0400

    Add us-east-2 AMI.

--- variable.tf
+++ variable.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "us-west-2"
+  default     = "us-east-2"
 }
 
 # Ubuntu Precise 12.04 LTS (x64)
@@ -8,6 +8,7 @@ variable "aws_amis" {
   default = {
     eu-west-1 = "ami-674cbc1e"
     us-east-1 = "ami-1d4e7a66"
+    us-east-2 = "ami-d80c35bd"
     us-west-1 = "ami-969ab1f6"
     us-west-2 = "ami-067f5c3d5a99edc80"
   }
